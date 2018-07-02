a7108b30a6e78fa9faded8468563548e080037fc
BartoszMyrcha <bartosz.myrcha@gmail.com>
2018-07-02 14:59:03 +0200

    Initial commit

---
+++ Assets/Activator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    Renderer cr;
+    public KeyCode key;
+    public static bool active;
+    GameObject note;
+
+    void Awake()
+    {
+        cr = GetComponent<Renderer>();
+        PlayerPrefs.SetInt("Points", 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            StartCoroutine(Pressed());
+            if (active)
+            {
+                Destroy(note);
+                AddPoint();
+                active = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        active = true;
+        if (col.gameObject.tag == "Note")
+            note = col.gameObject;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        active = false;
+    }
+
+    void AddPoint()
+    {
+        PlayerPrefs.SetInt("Points", PlayerPrefs.GetInt("Points") + 1);
+    }
+
+    IEnumerator Pressed()
+    {
+        Color old = cr.material.color;
+        cr.material.color = new Color(0, 255, 0);
+        yield return new WaitForSeconds(0.05f);
+        cr.material.color = old;
+    }
+}
---
+++ Assets/Neck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neck : MonoBehaviour {
+
+    public Material frontPlane;
+
+    // Use this for initialization
+    void Start () {
+        // Create the Plane
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        // Load the Image from somewhere on disk
+        var filePath = "C:/Users/Bartosz/Documents/para/Assets/Resources/Textures/bg.jpg";
+        if (System.IO.File.Exists(filePath))
+        {
+            // Image file exists - load bytes into texture
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+            var tex = new Texture2D(1, 1);
+            tex.LoadImage(bytes);
+            frontPlane.mainTexture = tex;
+
+            // Apply to Plane
+            MeshRenderer mr = plane.GetComponent<MeshRenderer>();
+            mr.material = frontPlane;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
